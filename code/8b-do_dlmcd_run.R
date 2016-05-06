27195b95488fa30b65c6ca4022211aa4278ec483
Devin Incerti <devin.incerti@gmail.com>
2016-05-05 22:56:50 -0700

    Correct number of iterations in Gibbs Sampler

--- code/8b-do_dlmcd_run.R
+++ code/8b-do_dlmcd_run.R
@@ -1,5 +1,5 @@
 # RUN DISTRICT LEVEL DLM WITH PRIOR -------------------------------------------
 # packages doSNOW and parallel are needed for running gibbs sampler
 # in parallel
-dlm.sims <- parallelGibbs(prior = TRUE, nsims = 2)
+dlm.sims <- parallelGibbs(prior = TRUE, nsims = 6000)
 save(dlm.sims, file = "output/dlmPosterior.Rdata")  
